be7fa67820a8f1e55a1a8a99d1333b49500fec7f
Ale2301 <alejo.2001@live.com.ar>
2024-06-24 15:27:20 -0300

    feat: death screen in-game

--- Files/test.js
+++ Files/test.js
@@ -1,9 +1,9 @@
 console.log("si ves esto el programador no hizo bien su trabajo");
 
-const phrases = ["te moriste", "alto puto, te moriste"];
 function pickRandom(max) {
   return Math.floor(Math.random() * max);
 }
 function getDeathScreenPhrase() {
+  const phrases = ["te moriste", "alto puto, te moriste"];
   return phrases[pickRandom(phrases.length)];
 }
