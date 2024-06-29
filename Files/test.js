2d2fd90484775bc92a49390b763bd6ec30ba44fa
SrTurco <luchodivincensi@gmail.com>
2024-06-28 21:59:56 -0300

    feat: bestiario(falta el texto) y frases de muerte

--- Files/test.js
+++ Files/test.js
@@ -4,6 +4,6 @@ function pickRandom(max) {
   return Math.floor(Math.random() * max);
 }
 function getDeathScreenPhrase() {
-  const phrases = ["te moriste", "alto puto, te moriste"];
+  const phrases = ["Mmm... ese es todo tu poder?", "Ya veo, ese es tu limite...","Nisiquiera puedo sentir lastima","Tu destino ya no te pertenece","Genial, ahora de nuevo...","Tu padre estaria decepcionado","Eso es todo lo que puedes hacer?","Lo haras una y mil veces y fallaras en todas","Ve y dile a tu padre que crio un perdedor","ya no puedo ni mirarte, no lo mereces","El frio toque de la muerte...","Puedo sentir como tu cuerpo se pone frio... Me gusta","Yo te adverti de que era impocible, no escuchaste","Tu destino se te escapa de tu manos... ","Eres un fracaso","Al final todos nos quedamos sin tiempo...","Eres un ser de vida inferior... solo rindete","Fuiste advertido de este final","Los dos sabiamos que esto acabaria asi","Al final solo eres polvo..."];
   return phrases[pickRandom(phrases.length)];
 }
